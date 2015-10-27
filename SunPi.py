5b2cc0de6f10b1b7e2c9d2a98a2babfb763a9389
Marcus Therkildsen <marcustherkildsen@gmail.com>
2015-10-27 18:11:40 +0100

    Cleaned to meet the pep8 standard

--- SunPi.py
+++ SunPi.py
@@ -5,12 +5,16 @@ from datetime import datetime, timedelta
 import plotly.plotly as py
 from plotly.graph_objs import Scatter, Layout, Figure, YAxis, Font, Marker
 import numpy as np
-from matplotlib.dates import date2num#,num2date
-from expected_solar import *
+from matplotlib.dates import date2num  # ,num2date
+from expected_solar import expected_solar
+
+'''
+SunPi script
+'''
 
 
-### SunPi script
 def connect2plotly():
+
     # Logging in to plotly
     username = 'your username'
     api_key = 'your api key'
@@ -24,87 +28,86 @@ def connect2plotly():
     trace1 = Scatter(
         x=[],
         y=[],
-        name = 'Wattage',
+        name='Wattage',
         stream=dict(
             token=stream_token,
             maxpoints=400
         ),
-        marker = Marker(
-        color = '#1f77b4'
-    ),
-    yaxis = 'y'
-    )
-
+        marker=Marker(
+            color='#1f77b4'
+            ),
+        yaxis='y'
+        )
 
     trace2 = Scatter(
         x=[],
         y=[],
-        name = 'Voltage',
+        name='Voltage',
         stream=dict(
             token=stream_token2,
             maxpoints=400
         ),
-        marker = Marker(
-        color = '#d62728'
-    ),
-    yaxis = 'y2'
-    )
+        marker=Marker(
+            color='#d62728'
+            ),
+        yaxis='y2'
+        )
 
     trace3 = Scatter(
         x=[],
         y=[],
-        name = 'Sun altitude',
+        name='Sun altitude',
         stream=dict(
             token=stream_token3,
             maxpoints=100
         ),
-        marker = Marker(
-        color = '#E79726'
-    ),
-    yaxis = 'y3'
-    )
-
+        marker=Marker(
+            color='#E79726'
+            ),
+        yaxis='y3'
+        )
 
     layout = Layout(
         title='SunPi battery',
         yaxis=YAxis(
             title='Power [W]',      # y-axis title
-        titlefont=Font(
-            color='#1f77b4'
+            titlefont=Font(
+                color='#1f77b4'
+                ),
+            tickfont=Font(
+                color='#1f77b4'
+                )
             ),
-        tickfont=Font(
-            color='#1f77b4'
-            )
-    ),
         yaxis2=YAxis(
-            title = 'Voltage [V]',
+            title='Voltage [V]',
             anchor='x',
             overlaying='y',
             side='right',
-        titlefont=Font(
-            color='#d62728'
-            ),
-        tickfont=Font(
-            color='#d62728'
-    )),
+            titlefont=Font(
+                color='#d62728'
+                ),
+            tickfont=Font(
+                color='#d62728'
+            )),
         yaxis3=YAxis(
-            title = 'Sun altitude [deg.]',
+            title='Sun altitude [deg.]',
             anchor='free',
             overlaying='y',
             side='right',
             position=0.95,
-        titlefont=Font(
-            color='#E79726'
+            titlefont=Font(
+                color='#E79726'
+                ),
+            tickfont=Font(
+                color='#E79726'
             ),
-        tickfont=Font(
-            color='#E79726'
-    ),
-    )
+        )
     )
 
-    fig = Figure(data=[trace1,trace2,trace3], layout=layout)
+    fig = Figure(data=[trace1, trace2, trace3], layout=layout)
 
-    # Comment this line (put # in front) to keep old data in plot when restarting
+    # Comment this line (put # in front)
+    # to keep old data in plot when restarting
     print py.plot(fig, filename='SunPi')
 
     # Open streams
@@ -123,19 +126,27 @@ def connect2plotly():
 [stream, stream2, stream3] = connect2plotly()
 
 # Adding hours
-add_hour = 0#2
+add_hour = 0
 
 # Values
 sec_between_real_measurements = 1
-num_loop_over_time = 216-16 #org 216-16 Number of measurements needed before taking avg for points (-16 since they are reserved for sending the mean to plotly)
-num_of_points_12_hours = 200 #org 200 Number of these avg points I want in 12 hours plot
-how_long_time_getting_data_for_one_point_should_have_taken = num_loop_over_time + 16
+
+# Org 216-16 Number of measurements needed before taking avg for points
+# (-16 since they are reserved for sending the mean to plotly)
+num_loop_over_time = 216-16
+
+# Org 200 Number of these avg points I want in 12 hours plot
+num_of_points_12_hours = 200
+
+expected_time = num_loop_over_time + 16
 
 # Initializing arrays
 loop_over_time = xrange(num_loop_over_time)
 bat_vol = np.zeros(num_loop_over_time)
 bat_wat = np.zeros(num_loop_over_time)
-full_day_data = np.empty([2*num_of_points_12_hours,2*num_of_points_12_hours]) # 400 points when having 216 sec in between
+# 400 points when having 216 sec in between
+full_day_data = np.empty([2*num_of_points_12_hours,
+                          2*num_of_points_12_hours])
 full_day_for_loop = xrange(2*num_of_points_12_hours)
 bat_vol_plotly = np.zeros(2*num_of_points_12_hours)
 bat_wat_plotly = np.zeros(2*num_of_points_12_hours)
@@ -147,7 +158,7 @@ ina = INA219()
 
 # Plotting what we expect
 try:
-    #print 'stream3'
+    # print 'stream3'
     time_expect, sun_expect = expected_solar()
     stream3.write({'x': time_expect, 'y': sun_expect})
 except Exception:
@@ -156,7 +167,7 @@ except Exception:
 
 
 # Getting start time
-starttime=time.time()
+starttime = time.time()
 
 g = -1
 
@@ -165,43 +176,45 @@ while True:
     # Collecting data over max 1 whole day
     # 400 points for a whole day
 
-    day_data_collected_from = (datetime.now() + timedelta(hours=add_hour)).date()
+    day_data_collected_from = (datetime.now() +
+                               timedelta(hours=add_hour)).date()
 
     # Collecting data for a single point
     for k in loop_over_time:
 
-    # Battery sensor
+        # Battery sensor
         try:
-            bat_vol[k] = (ina.getBusVoltage_V() + (ina.getShuntVoltage_mV()/1000)) # Voltage
-            bat_wat[k] = -bat_vol[k]*ina.getCurrent_mA()/1000 #bat_vol[k]*amp_temp,  Wattage
+            # Voltage
+            bat_vol[k] = (ina.getBusVoltage_V() +
+                          (ina.getShuntVoltage_mV()/1000))
+
+            # Wattage, bat_vol[k]*amp_temp
+            bat_wat[k] = -bat_vol[k]*ina.getCurrent_mA()/1000
         except Exception:
-            # If not able to read from device, simply pass. Oterwise the script will stop executing.
+            # If not able to read from device, simply pass.
+            # Oterwise the script will stop executing.
             pass
 
-        # Waiting a certain time such that the stuff inside this loop + time.sleep equals the sec_between_real_measurements
-        time.sleep(sec_between_real_measurements - ((time.time() - starttime) % sec_between_real_measurements))
+        # Waiting a certain time such that the stuff inside this loop +
+        # time.sleep equals the sec_between_real_measurements
+        time.sleep(sec_between_real_measurements -
+                   ((time.time() - starttime) % sec_between_real_measurements))
 
-    # Adding hours to fit timezone and/or summertime. Change this to your needs.
+    # Adding hours to fit timezone and/or summertime. Change this to your needs
     added_hours = datetime.now() + timedelta(hours=add_hour)
-    time_for_plotly = format(added_hours,'%Y-%m-%d %H:%M:%S.%f')
+    time_for_plotly = format(added_hours, '%Y-%m-%d %H:%M:%S.%f')
     bat_wat_plotly[g] = np.mean(bat_wat)
     bat_vol_plotly[g] = np.mean(bat_vol)
 
     # Since the time is a datetime object, translate this into numbers.
-    # When you want to go from these numbers to the original dates then use num2date
+    # When you want to go from these numbers to the original dates
+    # then use num2date
     time_for_saving[g] = date2num(added_hours)
-
-    # If battery is charged, switch relay on to run stuff
-    #print 'bat_vol_plotly is '+ str(bat_vol_plotly[g])
-    if bat_vol_plotly[g] > 13.6 and bat_vol_plotly[g-1] > 13.6 and bat_vol_plotly[g-2] > 13.6:
-        all_on_relay()
-    elif bat_vol_plotly[g] < 13 and bat_vol_plotly[g-1] < 13 and bat_vol_plotly[g-2] < 13:
-        all_off_relay()
 
     # Give loop some time to send the data to plotly in a consistent way
     # If exception: wait, reconnect
     try:
-        #print 'stream1'
+        # print 'stream1'
         stream.write({'x': time_for_plotly, 'y': bat_wat_plotly[g]})
     except Exception:
 
@@ -213,7 +226,7 @@ while True:
         pass
 
     try:
-        #print 'stream2'
+        # print 'stream2'
         stream2.write({'x': time_for_plotly, 'y': bat_vol_plotly[g]})
     except Exception:
 
@@ -224,18 +237,27 @@ while True:
 
         pass
 
-        # So the "Collecting data" loop should have taken sec_between_real_measurements * num_loop_over_time seconds
-        # Let's just collect data (for a single point) for 200 seconds, then spend 16 seconds on sending the mean of the data to plotly.
-        # This means, change num_loop_over_time from 216 -> 200
-
+    '''
+     So the "Collecting data" loop should have taken
+     sec_between_real_measurements * num_loop_over_time seconds
+     Let's just collect data (for a single point) for 200 seconds,
+     then spend 16 seconds on sending the mean of the data to plotly.
+     This means, change num_loop_over_time from 216 -> 200
+     '''
 
     # Check if we passed midnight and if: yes; then save data to txt
 
-    if day_data_collected_from < (datetime.now() + timedelta(hours=add_hour)).date():
-        full_day_data = [np.copy(time_for_saving),np.copy(bat_wat_plotly),np.copy(bat_vol_plotly)]
+    if day_data_collected_from < (datetime.now() +
+                                  timedelta(hours=add_hour)).date():
+
+        full_day_data = [np.copy(time_for_saving),
+                         np.copy(bat_wat_plotly),
+                         np.copy(bat_vol_plotly)]
 
         # Got data, now saving data
-        np.savetxt('/home/pi/SunPi/Data/SunPi_mismatch_'+day_data_collected_from.strftime("%Y_%m_%d")+'.txt',full_day_data)#test)
+        np.savetxt('/home/pi/SunPi/Data/SunPi_mismatch_' +
+                   day_data_collected_from.strftime("%Y_%m_%d") +
+                   '.txt', full_day_data)
 
         # Make sure the loop starts from g = 0 after saving files
         g = -1
@@ -244,15 +266,18 @@ while True:
         time_for_saving = np.zeros(2*num_of_points_12_hours)
         bat_vol_plotly = np.zeros(2*num_of_points_12_hours)
         bat_wat_plotly = np.zeros(2*num_of_points_12_hours)
-        full_day_data = np.empty([2*num_of_points_12_hours,2*num_of_points_12_hours])
+        full_day_data = np.empty([2*num_of_points_12_hours,
+                                  2*num_of_points_12_hours])
 
         # Plotting what we expect
         try:
-            #print 'stream3'
+            # print 'stream3'
             time_expect, sun_expect = expected_solar()
             stream3.write({'x': time_expect, 'y': sun_expect})
         except Exception:
             pass
-           # unable to get sun curve for today. oh, well. we will try again tomorrow !
-    # Again we wait such that the stuff inside loop + time.sleep equals how_long_time_getting_data_for_one_point_should_have_taken
-    time.sleep(how_long_time_getting_data_for_one_point_should_have_taken - ((time.time() - starttime) % how_long_time_getting_data_for_one_point_should_have_taken))
+            # Unable to get sun curve for today.
+            # Oh, well. we will try again tomorrow !
+
+    # Again we wait such that the stuff inside loop+time.sleep=expected_time
+    [time.sleep(expected_time - ((time.time() - starttime) % expected_time))]
--- expected_solar.py
+++ expected_solar.py
@@ -8,12 +8,14 @@ from __future__ import division
 import numpy as np
 from datetime import datetime
 from pytz import timezone
-import cookielib, urllib2
+import cookielib
+import urllib2
 import dateutil
 
-###############################################################################
-################################## CALC #######################################
-###############################################################################
+'''
+CALC
+'''
+
 
 def expected_solar():
 
@@ -23,26 +25,35 @@ def expected_solar():
     # Find your latitude and longitude with www.google.dk/maps/
     lat = 'your latitude'
     lon = 'your longitude'
-    
-    which_date = datetime.now().strftime("%d-%m-%Y") # today
-    daylight_saving = str(datetime.now(tz=timezone('CET')).timetuple().tm_isdst) # 0 for wintertime, 1 for summertime
-    
-    # url to get csv file from
-    url_data = 'http://www.solartopo.com/services/solarOrbit.php?lat='+lat+'&long='+lon+'&date='+which_date+'&dst='+daylight_saving
 
-    #download csv fil, åben og hent tid og højden ud
+    # Today
+    which_date = datetime.now().strftime("%d-%m-%Y")
+    # 0 for wintertime, 1 for summertime
+    daylight_saving = [str(datetime.now(tz=timezone('CET')).timetuple()
+                                                           .tm_isdst)]
+
+    # url to get csv file from
+    url_data = ['http://www.solartopo.com/services/solarOrbit.php?lat=' + lat +
+                '&long=' + lon + '&date=' + which_date + '&dst=' +
+                daylight_saving]
+
+    # Download csv file, open and extract time and height
     cj = cookielib.CookieJar()
     opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
     request = urllib2.Request(url_data)
-    response = opener.open(request)    
+    response = opener.open(request)
     data = response.read()
     with open('sun_height.csv', 'wb') as code:
         code.write(data)
 
-    sun_height_full = np.genfromtxt('sun_height.csv', delimiter=',', dtype='str',autostrip = True)[7:]
-    sun_height_full = np.array([sun_height_full[i].split(';') for i in xrange(len(sun_height_full))])[:,:2]    
-    
-    time_data = [dateutil.parser.parse(sun_height_full[i,0]) for i in xrange(len(sun_height_full[:,0]))]    
-    sun_alti = sun_height_full[:,1].astype(float)
+    sun_height_full = np.genfromtxt('sun_height.csv', delimiter=',',
+                                    dtype='str', autostrip=True)[7:]
+    sun_height_full = np.array([sun_height_full[i].split(';')
+                                for i in xrange(len(sun_height_full))])[:, :2]
+
+    time_data = [dateutil.parser.parse(sun_height_full[i, 0])
+                 for i in xrange(len(sun_height_full[:, 0]))]
+
+    sun_alti = sun_height_full[:, 1].astype(float)
 
     return time_data, sun_alti
